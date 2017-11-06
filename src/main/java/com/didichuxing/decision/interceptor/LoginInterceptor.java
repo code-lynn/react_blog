402f9076bb6160ed29f144d579bf57907ddc7474
wangzhimingjimmy <wangzhimingjimmy@didichuxing.com>
2017-11-06 20:02:24 +0800

    SSO

--- src/main/java/com/didichuxing/decision/interceptor/LoginInterceptor.java
+++ src/main/java/com/didichuxing/decision/interceptor/LoginInterceptor.java
@@ -3,6 +3,7 @@ package com.didichuxing.decision.interceptor;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.didichuxing.decision.service.SSOService;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,7 +36,7 @@ public class LoginInterceptor extends HandlerInterceptorAdapter{
         String ticket = ssoService.getTicketFromCookie(request, response);
         String username = ssoService.getUsernameFromCookie(request, response);
 
-        if (ticket == null){
+        if (ticket == null || StringUtils.isEmpty(ticket)){
             String loginUrl = ssoService.loginRequired(currentUrl);
             LOGGER.error("loginUrl =======> " + loginUrl);
             response.sendRedirect(loginUrl);
