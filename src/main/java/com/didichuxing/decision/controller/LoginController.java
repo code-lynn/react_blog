1247730696407d77589c037c82693789832afcba
wangzhimingjimmy <wangzhimingjimmy@didichuxing.com>
2017-11-06 19:54:47 +0800

    SSO

--- src/main/java/com/didichuxing/decision/controller/LoginController.java
+++ src/main/java/com/didichuxing/decision/controller/LoginController.java
@@ -18,9 +18,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.WebUtils;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * Created by didi on 2017/11/1.
@@ -45,48 +48,69 @@ public class LoginController {
     }
 
     @RequestMapping(value = "/oceanus/login/callback", method = RequestMethod.GET)
-    public String login(HttpServletRequest request,
-                        HttpServletResponse response,
-                        @RequestParam("jumpto") String jumpto,
-                        @RequestParam("code") String code) {
-        System.out.println("------------------> /login/callback");
-        String currentUrl = request.getRequestURI();
-        String checkCodeUrl = ssoService.getCallbackUrl();
-        RestTemplate restTemplate = new RestTemplateBuilder().build();
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Content-type", "application/x-www-form-urlencoded");
-        headers.add("Accept", "application/json");
-        JSONObject params = new JSONObject();
-        params.put("code", code);
-        params.put("app_id", ssoService.getJetAppId());
-        params.put("app_key", ssoService.getAppKey());
+    public String login(HttpServletRequest request, HttpServletResponse response) {
 
-        LOGGER.error("params ========> " + params.toString());
+        boolean result = false;
+        String redirect = null;
 
-        LOGGER.error("checkCodeUrl =========> " + checkCodeUrl);
-        String result = restTemplate.postForEntity(checkCodeUrl,
-            new HttpEntity<>(params.toString(), headers),
-            String.class).getBody();
-
-        LOGGER.error("result =========> " + result);
-        JSONObject loginInfo = JSON.parseObject(result);
-        if (loginInfo.getIntValue("errno") == 0){
-            String ticket = loginInfo.getJSONObject("data").getString("ticket");
-            String username = loginInfo.getJSONObject("data").getString("username");
-            LOGGER.error("ticke={}, username={}", ticket, username);
-            ssoService.setUserCookie(request, response, ticket, username);
-
-//            return "redirect:" + Const.MAIN_INDEX;
-            if(StringUtils.isBlank(jumpto) || jumpto.equals("index")){
-                return "redirect:" + Const.MAIN_INDEX;
-            }else{
-                return "redirect:" + jumpto;
+        try {
+            result = checkCode(request, response);
+        } catch (IOException e) {
+            LOGGER.error("IOException", e);
+        } catch (ServletException e) {
+            LOGGER.error("ServletException", e);
+        }
+        if (result) {
+            String jumpto = null;
+            jumpto = WebUtils.findParameterValue(request, "jumpto");
+            if (jumpto != null) {
+                redirect = "redirect:" + jumpto;
+            } else {
+                redirect = "redirect:" + mainIndex;
             }
+        } else {
+            LOGGER.error("code error ");
         }
-        else {
-            String loginUrl = ssoService.loginRequired(currentUrl);
-            return "redirect: " + loginUrl;
-        }
+        return redirect;
+
+//        System.out.println("------------------> /login/callback");
+//        String currentUrl = request.getRequestURI();
+//        String checkCodeUrl = ssoService.getCallbackUrl();
+//        RestTemplate restTemplate = new RestTemplateBuilder().build();
+//        HttpHeaders headers = new HttpHeaders();
+//        headers.add("Content-type", "application/x-www-form-urlencoded");
+//        headers.add("Accept", "application/json");
+//        JSONObject params = new JSONObject();
+//        params.put("code", code);
+//        params.put("app_id", ssoService.getJetAppId());
+//        params.put("app_key", ssoService.getAppKey());
+//
+//        LOGGER.error("params ========> " + params.toString());
+//
+//        LOGGER.error("checkCodeUrl =========> " + checkCodeUrl);
+//        String result = restTemplate.postForEntity(checkCodeUrl,
+//            new HttpEntity<>(params.toString(), headers),
+//            String.class).getBody();
+//
+//        LOGGER.error("result =========> " + result);
+//        JSONObject loginInfo = JSON.parseObject(result);
+//        if (loginInfo.getIntValue("errno") == 0){
+//            String ticket = loginInfo.getJSONObject("data").getString("ticket");
+//            String username = loginInfo.getJSONObject("data").getString("username");
+//            LOGGER.error("ticke={}, username={}", ticket, username);
+//            ssoService.setUserCookie(request, response, ticket, username);
+//
+////            return "redirect:" + Const.MAIN_INDEX;
+//            if(StringUtils.isBlank(jumpto) || jumpto.equals("index")){
+//                return "redirect:" + Const.MAIN_INDEX;
+//            }else{
+//                return "redirect:" + jumpto;
+//            }
+//        }
+//        else {
+//            String loginUrl = ssoService.loginRequired(currentUrl);
+//            return "redirect: " + loginUrl;
+//        }
     }
 
     @RequestMapping(value = "/oceanus/logout", method = RequestMethod.GET)
@@ -94,5 +118,31 @@ public class LoginController {
         ssoService.clearCookies(request, response);
         String logoutUrl = ssoService.getLogoutUrl();
         return "redirect:" + logoutUrl;
+    }
+
+    private boolean checkCode(HttpServletRequest request, HttpServletResponse response) throws IOException,
+        ServletException {
+        String code = WebUtils.findParameterValue(request, "code");
+        if (StringUtils.isNotEmpty(code)) {
+            String result = ssoService.validCode(code);
+            JSONObject rootObject = JSON.parseObject(result);
+            Integer error = rootObject.getInteger("errno");
+            if (error != null) {
+                JSONObject dataObject = rootObject.getJSONObject("data");
+                if (dataObject != null) {
+                    String ticket = dataObject.getString("ticket");
+                    String userName = dataObject.getString("username");
+                    if (StringUtils.isEmpty(ticket) || StringUtils.isEmpty(userName)) {
+                        LOGGER.error("ticket or username empty");
+                        return false;
+                    }
+                    ssoService.setUserCookie(request, response, ticket, userName);
+                    return true;
+                }
+            }
+        } else {
+            LOGGER.error("code empty");
+        }
+        return false;
     }
 }
--- src/main/java/com/didichuxing/decision/interceptor/LoginInterceptor.java
+++ src/main/java/com/didichuxing/decision/interceptor/LoginInterceptor.java
@@ -3,18 +3,15 @@ package com.didichuxing.decision.interceptor;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.didichuxing.decision.service.SSOService;
-import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
-import org.springframework.web.util.WebUtils;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -37,48 +34,48 @@ public class LoginInterceptor extends HandlerInterceptorAdapter{
         }
         String ticket = ssoService.getTicketFromCookie(request, response);
         String username = ssoService.getUsernameFromCookie(request, response);
-        JSONObject params = new JSONObject();
-        params.put("ticket", ticket);
-        params.put("app_id", ssoService.getJetAppId());
 
-        String checkTicketUrl = ssoService.getTicketUrl();
-        RestTemplate restTemplate = new RestTemplateBuilder().build();
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Content-type", "application/x-www-form-urlencoded");
-        headers.add("Accept", "application/json");
-
-        LOGGER.error("params ========> " + params.toString());
-        LOGGER.error("currentUrl =========> " + currentUrl);
-        LOGGER.error("checkTicketUrl =========> " + checkTicketUrl);
-        String result = restTemplate.postForEntity(checkTicketUrl,
-            new HttpEntity<>(params.toString(), headers),
-            String.class).getBody();
-
-        LOGGER.error("checkTicketUrl result =======>" + result);
-        JSONObject resultInfo = JSON.parseObject(result);
-
-        if (resultInfo.getIntValue("errno") == 1){
-//            if (currentUrl.equals("/oceanus/login/callback")) {
-                String loginUrl = ssoService.loginRequired(currentUrl);
-                LOGGER.error("loginUrl ========> " + loginUrl);
-                response.sendRedirect(loginUrl);
-                return true;
-//            }
-//            else {
-//                return false;
-//            }
-//            if (!currentUrl.equals("/oceanus/login/callback")) {
-//                String loginUrl = ssoService.loginRequired(currentUrl);
-//                LOGGER.error("loginUrl ========> " + loginUrl);
-//                response.sendRedirect(loginUrl);
-//                return false;
-//            }
-//            else {
-//            }
+        if (ticket == null){
+            String loginUrl = ssoService.loginRequired(currentUrl);
+            response.sendRedirect(loginUrl);
+            return false;
         }
         else {
+            ssoService.validTicket(ticket);
             return true;
         }
+
+
+
+//        JSONObject params = new JSONObject();
+//        params.put("ticket", ticket);
+//        params.put("app_id", ssoService.getJetAppId());
+//
+//        String checkTicketUrl = ssoService.getTicketUrl();
+//        RestTemplate restTemplate = new RestTemplateBuilder().build();
+//        HttpHeaders headers = new HttpHeaders();
+//        headers.add("Content-type", "application/x-www-form-urlencoded");
+//        headers.add("Accept", "application/json");
+//
+//        LOGGER.error("params ========> " + params.toString());
+//        LOGGER.error("currentUrl =========> " + currentUrl);
+//        LOGGER.error("checkTicketUrl =========> " + checkTicketUrl);
+//        String result = restTemplate.postForEntity(checkTicketUrl,
+//            new HttpEntity<>(params.toString(), headers),
+//            String.class).getBody();
+//
+//        LOGGER.error("checkTicketUrl result =======>" + result);
+//        JSONObject resultInfo = JSON.parseObject(result);
+//
+//        if (resultInfo.getIntValue("errno") == 1){
+//            String loginUrl = ssoService.loginRequired(currentUrl);
+//            LOGGER.error("loginUrl ========> " + loginUrl);
+//            response.sendRedirect(loginUrl);
+//            return false;
+//        }
+//        else {
+//            return true;
+//        }
     }
 
     @Override
